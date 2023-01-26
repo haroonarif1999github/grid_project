454c5beaafb643a1a00d6179aea45dcf168d68a8
haroon <haroonarif1999github@gmail.com>
2023-01-25 20:03:04 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import MediaqueryPractise from './MediaqueryPractise';
+
+function App() {
+  return (
+<>
+    <div className='container'>
+      
+      <div className="item item1">item 1</div>
+      <div className="item item2">item 2</div>
+      <div className="item item3">item 3</div>
+      <div className="item item4">item 4</div>
+      <div className="item item5">item 5</div>
+      <div className="item item6">item 6</div>
+      <div className="item item7">item 7</div>
+      <div className="item item8">item 8</div>
+      <div className="item item9">item 9</div>
+      <div className="item item10">item 10</div>
+      <div className="item item11">item 11</div>
+      <div className="item item12">item 12</div>
+    </div>
+    <MediaqueryPractise/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MediaqueryPractise.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './MediaqueryPractise.css'
+const MediaqueryPractise = () => {
+  return (
+    <div className='body'>
+      <header className='border'>Header</header>
+      <main className='border'>
+      <div className="1">1</div>
+      <div className="2">2</div>
+      <div className="3">3</div>
+      <div className="4">4</div>
+      <div className="5">5</div>
+      <div className="6">6</div>
+      </main>
+      <aside className='border'>Sidebar</aside>
+      <footer className='border'>Footer</footer>
+    </div>
+  )
+}
+
+export default MediaqueryPractise
